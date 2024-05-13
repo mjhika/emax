bbf2bd1f2f56e6509c0d16471c401c744e9ec88a
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-05-13 17:32:40 -0400

    add crux

--- init.el
+++ init.el
@@ -60,6 +60,14 @@
   :config
   (direnv-mode))
 
+(use-package crux
+  :ensure t
+  :config
+  (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+  (global-set-key [(super return)] #'crux-smart-open-line-above)
+  (global-set-key (kbd "C-<return>") #'crux-smart-open-line)
+  (global-set-key (kbd "C-c g x") #'crux-view-url))
+
 (use-package projectile
   :ensure t
   :bind ("s-p" . projectile-command-map)
@@ -190,7 +198,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-modeline projectile direnv yasnippet company flycheck lsp-ui lsp-mode cider clojure-mode orderless smartparens which-key vertico)))
+   '(crux doom-modeline projectile direnv yasnippet company flycheck lsp-ui lsp-mode cider clojure-mode orderless smartparens which-key vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
