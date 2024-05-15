3fb16dc5b77cedcc624138950ae2a7f1a935efde
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-05-14 21:25:08 -0400

    enable menu-bar-mode

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
       ring-bell-function 'ignore
       read-process-output-max (* 1024 1024))
 
-(menu-bar-mode -1)
+(menu-bar-mode 1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (pixel-scroll-precision-mode 1)
