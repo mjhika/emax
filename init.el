31c90f104677c800e8160b2928f13accbba774e2
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-07-13 17:13:00 -0400

    remove erc config
    using halloy now

--- init.el
+++ init.el
@@ -296,18 +296,6 @@
 (bind-keys* ("s-f" . mjhika/open-init-file)
 	    ("C-x C-b" . ibuffer))
 
-(use-package erc
-  :ensure nil
-  :init
-  (setq erc-nick "mjhika"
-	erc-user-full-name "mjhika")
-
-  (defun mjhika/erc->libera ()
-    (interactive)
-    (erc-tls :server "irc.libera.chat"
-	     :port 6697
-	     :password (shell-command-to-string "bb -o '(-> (clojure.java.shell/sh \"op\" \"item\" \"get\" \"fwjq4wl52sancfp66bbu23zube\" \"--format\" \"json\") :out (json/parse-string keyword) :fields (nth 1) :value)'"))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
