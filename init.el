d941ab4b68e7d2eabf3f611d30d75c59ac235723
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-07-24 10:41:15 -0400

    remove quoting
    quoting is not necessary in cl-case

--- init.el
+++ init.el
@@ -15,8 +15,8 @@
   (run-with-idle-timer 2 t (lambda () (garbage-collect)))
 
   (cl-ecase system-type
-    ('darwin (set-frame-font "Berkeley Mono 12" nil t))
-    ('gnu/linux (set-frame-font "Berkeley Mono 10" nil t)))
+    (darwin (set-frame-font "Berkeley Mono 12" nil t))
+    (gnu/linux (set-frame-font "Berkeley Mono 10" nil t)))
 
   (menu-bar-mode 1)
   (scroll-bar-mode -1)
